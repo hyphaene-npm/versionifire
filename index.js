4bfa0e007da04fa7c2bbcb7af98e61304af92d83
Maximilien Garenne <maximilien.garenne@cheetahdigital.com>
2018-05-24 17:12:42 +0200

    add basic

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 #!/usr/bin/env node
+const fs = require('fs');
 
-const path = require('path');
+const acceptedArgs = ['patch', 'minor', 'help', 'major', 'version'];
+console.log('========');
 
-fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data) {
+console.log(process.argv);
+console.log('========');
+
+fs.readFile('./package.json', 'utf8', function readFileCallback(err, data) {
   if (err) {
     console.log(err);
+    throw 'oops';
   } else {
-    obj = JSON.parse(data); //now it an object
-    obj.table.push({ id: 2, square: 3 }); //add some data
-    json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile('myjsonfile.json', json, 'utf8', callback); // write it back
+    const package = JSON.parse(data); //now it an object
+    const { version } = package;
+    const [major, minor, patch] = version.split('.');
+    console.log({ major, minor, patch });
+    const json = JSON.stringify(package); //convert it back to json
+    fs.writeFile('myjsonfile.json', json, 'utf8'); // write it back
   }
 });
