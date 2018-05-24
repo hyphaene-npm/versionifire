e4807797b332d722d595478dc332f9b16e0b8f23
Maximilien Garenne <maximilien.garenne@cheetahdigital.com>
2018-05-24 17:17:52 +0200

    add test

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ fs.readFile('./package.json', 'utf8', function readFileCallback(err, data) {
   } else {
     const package = JSON.parse(data); //now it an object
     const { version } = package;
+    // test //
+    package.yolo = 'yolooooo';
     const [major, minor, patch] = version.split('.');
     console.log({ major, minor, patch });
     const json = JSON.stringify(package); //convert it back to json
