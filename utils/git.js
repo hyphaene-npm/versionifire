823de252d7fad68a6e4043d099c485c5f6994b12
Maximilien Garenne <maximilien.garenne@cheetahdigital.com>
2018-05-26 03:15:49 +0200

    update utils

--- utils/git.js
+++ utils/git.js
@@ -1,11 +1,20 @@
 const execa = require('execa');
 const ora = require('ora');
-const getCommitMessage = require('./index');
+const fs = require('fs');
+const path = require('path');
+const { getCommitMessage } = require('./index');
 //
 const getSpinner = text => ({ text, color: 'red' });
 //
-const getIsGitInitialized = async () =>
-	await execa.shell('git rev-parse --is-inside-work-tree 2>/dev/null').stdout;
+const getIsGitInitialized = () => {
+	const gitPath = path.join(process.cwd(), '.git');
+	try {
+		fs.readdirSync(gitPath);
+		return true;
+	} catch (e) {
+		return false;
+	}
+};
 //
 const initGit = async () => execa.shell('git init');
 
@@ -13,11 +22,23 @@ const commit = async (commitMessage, version) => {
 	execa.shell(`git commit -m "${getCommitMessage(commitMessage, version)}"`);
 };
 const add = async () => {
-	execa.shell('git add package.json');
+	execa.shell('git add ./package.json');
 };
 
-const pushToCurrentBranch = async () =>
-	execa.shell('git push origin `git rev-parse --abbrev-ref HEAD`');
+const pushToCurrentBranch = async (repo = 'origin') =>
+	execa.shell(`git push ${repo} "$(git rev-parse --abbrev-ref HEAD)"`);
+
+const oraInit = async () => {
+	const promise = initGit();
+	ora.promise(promise, getSpinner(`init`));
+	await promise;
+};
+
+const oraAdd = async () => {
+	const promise = add();
+	ora.promise(promise, getSpinner(`add on staging`));
+	await promise;
+};
 
 const oraCommit = async (commitMessage, version) => {
 	const promise = commit(commitMessage, version);
@@ -25,19 +46,9 @@ const oraCommit = async (commitMessage, version) => {
 	await promise;
 };
 
-const oraInit = async () => {
-	const promise = initGit();
-	ora.promise(promise, getSpinner(`add on staging`));
-	await promise;
-};
-const oraAdd = async () => {
-	const promise = add();
-	ora.promise(promise, getSpinner(`add on staging`));
-	await promise;
-};
 const oraPush = async () => {
 	const promise = pushToCurrentBranch();
-	ora.promise(promise, getSpinner(`add on staging`));
+	ora.promise(promise, getSpinner(`push`));
 	await promise;
 };
 
