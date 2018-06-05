abca2d793bcbbcb35665c68f8981fe1342bbd13f
Maximilien Garenne <maximilien.garenne@cheetahdigital.com>
2018-06-05 16:43:48 +0200

    add const

--- constants/index.js
+++ constants/index.js
@@ -17,12 +17,19 @@ const MINOR = 'minor';
 const MAJOR = 'major';
 const VERSION = 'version';
 const HELP = 'help';
+const ADD_SCRIPTS = 'scripts';
 const DEFAULT_OPTIONS = 'default';
-const ACCEPTED_ARGS = [PATCH, MINOR, HELP, MAJOR, VERSION, DEFAULT_OPTIONS];
+const ACCEPTED_ARGS = [PATCH, MINOR, HELP, MAJOR, VERSION, DEFAULT_OPTIONS, ADD_SCRIPTS];
 
 const AVAILABLE_COMMANDS = [PATCH, MINOR, MAJOR]
 	.map(x => `$ versionifier ${x} ( will update by increasing ${x} version)`)
 	.reduce((acc, current) => `${acc}\n${current}\n`, VERSION_COMMAND);
+
+const SCRIPTS = {
+	[PATCH]: `versionifier ${PATCH}`,
+	[MINOR]: `versionifier ${MINOR}`,
+	[MAJOR]: `versionifier ${MAJOR}`
+};
 
 const PACKAGE_PATH = './package.json';
 const GIT_PATH = './.git';
@@ -51,7 +58,9 @@ module.exports = {
 	MINOR,
 	MAJOR,
 	VERSION,
+	ADD_SCRIPTS,
 	HELP,
+	SCRIPTS,
 	DEFAULT_OPTIONS,
 	AVAILABLE_COMMANDS,
 	DEFAULT,
